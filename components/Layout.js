9f59451da7fc3ac4fa4db3c6838997df88b75079
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-08-15 12:56:33 -0300

    nova mudanca

--- components/Layout.js
+++ components/Layout.js
@@ -8,9 +8,27 @@ const Layout = (props) => (
       <meta name="description" content="Site para anotar os filmes que ja foram assistidos" />
       <meta name="author" content="Leao Labs" />
     </Head>
-    <div className="container">
-      {props.children}
-    </div>
+
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <a className="navbar-brand" href="#">Jassisti</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Filmes</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    {props.children}
+
   </div>
 );
 
